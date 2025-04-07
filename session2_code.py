71b7f798d3b69290536430258af4fc8849278eef
jwmckeown007 <jwmckeow@ucsc.edu>
2025-04-07 14:14:14 -0700

    Add files via upload

---
+++ session2_code.py
@@ -0,0 +1,6 @@
+sum_floats = 2.0 + 4.5
+diff_int = 5 - 3
+prod_floatint = 4.0 * 5
+print(f"{sum_floats}: {type(sum_floats)}")
+print(f"{diff_int}: {type(diff_int)}")
+print(f"{prod_floatint}: {type(prod_floatint)}")
