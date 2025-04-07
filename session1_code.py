24fe4b713faf89bb8c350444fea97af4cc7d634d
jwmckeown007 <jwmckeow@ucsc.edu>
2025-04-07 12:21:25 -0700

    Add files via upload

---
+++ session1_code.py
@@ -0,0 +1,4 @@
+print("Hi! My name is Joey McKeown.")
+print("I use he/him pronouns.")
+print("My favorite movie (at least right now) is The Lord of the Rings.\n", end="")
+print("My favorite food are burritos!")
