85d0cf38ddcc9b93705e38e1dd015740740bac97
jwmckeown007 <jwmckeow@ucsc.edu>
2025-04-07 17:59:01 -0700

    Add files via upload

---
+++ session4_code.py
@@ -0,0 +1,19 @@
+class Platypus:
+	def __init__(self, arm_length, leg_length, num_eyes, has_tail, is_furry):
+		self.arm_length = float(arm_length)
+		self.leg_length = float(leg_length)
+		self.num_eyes = int(num_eyes)
+		self.has_tail = bool(has_tail)
+		self.is_furry = bool(is_furry)
+
+	def characteristics(self):
+		print("Platypus Physical Characteristics:")
+		print(f"Arm Length: {self.arm_length}")
+		print(f"Leg Length: {self.leg_length}")
+		print(f"Number of Eyes: {self.num_eyes}")
+		print(f"Has a Tail? {"Yes" if self.has_tail == True else "No"}")
+		print(f"Is Furry? {"Yes" if self.is_furry == True else "No"}")
+
+
+Perry = Platypus(4.0, 4.2, 2, True, True)
+Perry.characteristics()
