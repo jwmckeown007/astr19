ee47b9bd37a17d91fc530d01907797b2b304f4cf
jwmckeown007 <jwmckeow@ucsc.edu>
2025-04-20 21:21:18 -0700

    session1_code.py

--- session1_code.py
+++ session1_code.py
@@ -1,4 +1,4 @@
 print("Hi! My name is Joey McKeown.")
 print("I use he/him pronouns.")
-print("My favorite movie (at least right now) is The Lord of the Rings.\n", end="")
-print("My favorite food are burritos!")
+print("My favorite movie (at least right now) is The Lord of the Rings – The Return of the King.", end="")
+print("My favorite food is burritos!")
