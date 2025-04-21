67ededf590fee79a04c923471baabedd3caf2f86
jwmckeown007 <jwmckeow@ucsc.edu>
2025-04-20 21:44:45 -0700

    session4_code.py

--- session4_code.py
+++ session4_code.py
@@ -15,5 +15,5 @@ class Platypus:
 		print(f"Is Furry? {'Yes' if self.is_furry else 'No'}")
 
 if __name__ == "__main__":
-	Perry = Platypus(4.0, 4.2, 2, True, True)
-	Perry.characteristics()
+	perry = Platypus(4.0, 4.2, 2, True, True)
+	perry.characteristics()
